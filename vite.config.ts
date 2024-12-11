df5923b911c2ce036680f2962406c43eeaa04acc
itsdannyt <itsdannytmedia@gmail.com>
2024-12-11 16:50:29 -0700

    Auto-commit: 2024-12-11 16:50:29

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,24 @@ export default defineConfig({
     host: true, // Listen on all local IPs
     port: 5174, // Default Vite port
     strictPort: true, // This will make Vite fail if port 5174 is not available instead of trying another port
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5174',
+        changeOrigin: true,
+        secure: false,
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        }
+      }
+    }
   },
   resolve: {
     alias: {
